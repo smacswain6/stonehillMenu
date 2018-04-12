3e46176db180d8acaffa3a9c46f487f431b07d94
Matt Peters <mpeters@students.stonehill.edu>
2018-04-11 21:42:27 -0400

    fooditem.html POST to be handled by fooditem.php

---
+++ php/fooditem.php
@@ -0,0 +1,17 @@
+<html>
+<body>
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Matt
+ * Date: 4/11/18
+ * Time: 9:24 PM
+ */
+
+/** This class will handle post requests for the admin page */
+
+include("../templates/fooditem.html");
+
+?>
+</body>
+</html>
