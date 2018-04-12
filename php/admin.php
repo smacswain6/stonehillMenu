8b7b015cb17713044c476e99a861f0729105ac8d
Matt Peters <mpeters@students.stonehill.edu>
2018-04-11 21:32:40 -0400

    added admin.php and adjusted admin.html to refer to admin.php

---
+++ php/admin.php
@@ -0,0 +1,19 @@
+<html>
+<body>
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Matt
+ * Date: 4/11/18
+ * Time: 9:24 PM
+ */
+
+/** This class will handle post requests for the admin page */
+
+include("../templates/admin.html");
+
+?>
+</body>
+</html>
+
+
