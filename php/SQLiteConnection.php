0b5426fe8a59e3dcbe163f80ef5c98f84de6341f
smacswain <smacswain@students.stonehill.edu>
2018-04-13 16:45:32 -0400

    MAde SQLiteConnection class and worked on userDao

---
+++ php/SQLiteConnection.php
@@ -0,0 +1,18 @@
+<?php
+class SQLiteConnection {
+
+private $pdo;
+
+/**
+* return in instance of the PDO object that connects to the SQLite database
+* @return \PDO
+*/
+public function connect() {
+if ($this->pdo == null) {
+$this->pdo = new \PDO("sqlite:" . "../db/database.db");
+}
+return $this->pdo;
+}
+}
+
+?>
--- php/UserDao.php
+++ php/UserDao.php
@@ -5,12 +5,30 @@
  * Date: 4/11/2018
  * Time: 9:32 PM
  */
+use SQLiteConnection;
 
 class UserDao extends SQLite3
 {
-    function __construct()
+    function connect()
     {
-        private $pdo=new PDO("sqlite:"."../db/user.db");
+        $pdo = (new SQLiteConnection())->connect();
+        if ($pdo != null) {
+            echo 'Connected to the SQLite database successfully!';
+        } else
+            echo 'Whoops, could not connect to the SQLite database!';
     }
 
+    function selectByUserID()
+    {
+        $stmt = $this->pdo->query('SELECT id, password '
+            . 'FROM users');
+        
+        while ($row = $stmt->fetch(\PDO::FETCH_ASSOC)) {
+            $projects[] = [
+                'project_id' => $row['project_id'],
+                'project_name' => $row['project_name']
+            ];
+        }
+        return $projects;
+    }
 }
