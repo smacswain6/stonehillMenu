766341ee11dd4c9c85be7fb71f1a56254e88bff6
smacswain <smacswain@students.stonehill.edu>
2018-04-16 20:49:57 -0400

    added in insert,delete,update methods for UserDao

--- php/UserDao.php
+++ php/UserDao.php
@@ -15,9 +15,10 @@ class UserDao extends SQLite3
      * @var PDO
      */
     private $pdo;
+
     public function __construct()
     {
-        $this->pdo=(new SQLiteConnection())->connect();
+        $this->pdo = (new SQLiteConnection())->connect();
     }
 
     public function connect()
@@ -31,26 +32,105 @@ class UserDao extends SQLite3
 
     function selectByUserID($userid)
     {
-        $stmt=$this->pdo->prepare("SELECT id,password FROM users WHERE id=:id;");
-        if($stmt==NULL) {
+        $stmt = $this->pdo->prepare("SELECT id,password FROM users WHERE id=:id;");
+        if ($stmt == NULL) {
             error_log("stmt is null", 0);
         }
         try {
             $stmt->execute([':id' => $userid]);
             $row = $stmt->fetch(\PDO::FETCH_ASSOC);
-        if($row==NULL)
-        {
-            print("Error in selectByUserID, user not found");
-            return NULL;
-        }
-        else {
-            $user = new User($row['id'], $row['password']);
-            return $user;
-        }
-        }
-        catch (PDOException $exception)
-        {
-          error_log($exception->getMessage());
+            if ($row == NULL) {
+                print("Error in selectByUserID, user not found");
+                return NULL;
+            } else {
+                $user = new User($row['id'], $row['password']);
+                return $user;
+            }
+        } catch (PDOException $exception) {
+            error_log($exception->getMessage());
         }
     }
+
+    function selectAll()
+    {
+        $stmt = $this->pdo->prepare("SELECT * from users;");
+        if ($stmt == NULL) {
+            error_log("stmt is null", 0);
+        }
+        try {
+            $stmt->execute();
+            $users = [];
+            while ($row = $stmt->fetch(\PDO::FETCH_ASSOC)) {
+                $user = new User($row['id'], $row['password']);
+                array_push($users, $user);
+            }
+            return $users;
+        } catch (PDOException $exception) {
+            error_log($exception->getMessage());
+        }
+        return Null;
+    }
+    function insert($user)
+    {
+        try {
+            $sql = 'INSERT INTO users(id,password) VALUES(:id,:password)';
+            $stmt = $this->pdo->prepare($sql);
+            $stmt->execute([
+                ':id' => $user->username,
+                ':password' => $user->password,
+            ]);
+            return true;
+        }
+        catch (PDOException $exception) {
+            error_log($exception->getMessage());
+            return false;
+        }
+    }
+    function update($user)
+    {
+        try {
+            $sql = 'UPDATE users set id = :id, password = :password';
+            $stmt = $this->pdo->prepare($sql);
+            $stmt->execute([
+                ':id' => $user->username,
+                ':password' => $user->password,
+            ]);
+            return true;
+        }
+        catch (PDOException $exception) {
+            error_log($exception->getMessage());
+            return false;
+        }
+    }
+    function delete($user)
+    {
+        try {
+            $sql = 'DELETE FROM users WHERE id=:id';
+            $stmt = $this->pdo->prepare($sql);
+            $stmt->execute([
+                ':id' => $user->username,
+            ]);
+            return true;
+        } catch (PDOException $exception) {
+            error_log($exception->getMessage());
+            return false;
+        }
+    }
+    function populate()
+    {
+        $dao=new UserDao();
+        $dao->insert(new User('sam','bradford'));
+        $dao->insert(new User('nick','lischenok'));
+        $dao->insert(new User('matt','young'));
+        $dao->insert(new User('michael','middleton'));
+    }
+
 }
+
+#UserDao::populate();
+$dao=new UserDao();
+//$dao->delete(new User("michael","middleton"));
+//$dao->delete(new User('nick','lischenok'));
+//$dao->delete(new User('matt','young'));
+//$dao->delete(new User('michael','middleton'));
+$dao->update(new User("sam","passwordChange"));
