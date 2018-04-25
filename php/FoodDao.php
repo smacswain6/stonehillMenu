46b51fb6f85b7327ca7d02c7314e2b7fa1abbe49
smacswain <smacswain@students.stonehill.edu>
2018-04-25 17:18:11 -0400

    removed old db file and added selectAll method to FoodDao

--- php/FoodDao.php
+++ php/FoodDao.php
@@ -33,8 +33,8 @@ class FoodDao
     public function selectByFoodname($foodname)
     {
         $stmt = $this->pdo->prepare("SELECT * from foods where name=:name;");
-        if($stmt == NULL){
-        error_log("stmt is null", 0);
+        if ($stmt == NULL) {
+            error_log("stmt is null", 0);
         }
         try {
             $stmt->execute([':name' => $foodname]);
@@ -51,4 +51,57 @@ class FoodDao
             error_log($exception->getMessage());
         }
     }
+    function selectAll()
+    {
+        $stmt = $this->pdo->prepare("SELECT * from foods;");
+        if ($stmt == NULL) {
+            error_log("stmt is null", 0);
+        }
+        try {
+            $stmt->execute();
+            $foodItems = [];
+            while ($row = $stmt->fetch(\PDO::FETCH_ASSOC)) {
+                $foodItem = new Food($row['name'], $row['rating'], $row['image'], $row['description'], $row['station'],
+                    $row['day'], $row['votes'], $row['current']);
+                print_r($foodItem);
+                array_push($foodItems, $foodItem);
+            }
+            return $foodItems;
+        } catch (PDOException $exception) {
+            error_log($exception->getMessage());
+        }
+        return Null;
+    }
+
+    function insert($foodItem)
+    {
+        try {
+            $sql = 'INSERT INTO foods(name,rating,image,description,station,day,votes,current) 
+                              VALUES(:name,:rating,:image,:description,:station,:day,:votes,:current);';
+            $stmt = $this->pdo->prepare($sql);
+            $stmt->execute([
+                ':name' => $foodItem->name,
+                ':rating' => $foodItem->rating,
+                ':image' => $foodItem->image,
+                ':description' => $foodItem->description,
+                ':station' => $foodItem->station,
+                ':day' => $foodItem->day,
+                ':votes' => $foodItem->votes,
+                ':current' => $foodItem->current,
+            ]);
+            return true;
+        } catch (PDOException $exception) {
+            error_log($exception->getMessage());
+            return false;
+        }
+    }
+
+    function populate()
+    {
+        $dao = new FoodDao();
+        $dao->insert(new Food('Chicken Wrap', 5, 'chickenwrap.jpg', 'crispy chicken wrap', 'Entree', 'Monday', 1, 'true'));
+    }
 }
+#FoodDao::populate();
+$dao=new FoodDao();
+$foods=$dao->selectAll();
