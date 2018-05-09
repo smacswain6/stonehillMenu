1dad1586e29ff9199eb87bb7103b96b75220d570
Matt Peters <mpeters@students.stonehill.edu>
2018-05-08 20:36:29 -0400

    added more reviews

--- php/ReviewDao.php
+++ php/ReviewDao.php
@@ -119,16 +119,59 @@ class ReviewDao
     public function populate()
     {
         $dao = new ReviewDao();
-        $dao->insert(new Review('matt','Not Bad','Baked Potato Bar'));
-        $dao->insert(new Review('nick','pretty good','Calzone'));
-        $dao->insert(new Review('sam','not good','Chicken Wrap'));
-        $dao->insert(new Review('stephen','aweful','Burger Mania'));
-        $dao->insert(new Review('michael','fantastic','Baked Potato Bar'));
+        $dao->insert(new Review('matt','Worst burger ive ever had','Angus Quesadilla Burger'));
+        $dao->insert(new Review('abby','Great','Angus Quesadilla Burger'));
+        $dao->insert(new Review('brock','Decent, ive had better','Angus Quesadilla Burger'));
+        $dao->insert(new Review('ryan','Best burger ive ever had','Angus Quesadilla Burger'));
+
+        $dao->insert(new Review('matt','Not Bad','Antipasto Salad Plate'));
+        $dao->insert(new Review('matthew','Too much dressing','Antipasto Salad Plate'));
+        $dao->insert(new Review('nick','Horrible','Antipasto Salad Plate'));
+        $dao->insert(new Review('viola','Fantastic','Antipasto Salad Plate'));
+        $dao->insert(new Review('vladimira','Too many croutons','Antipasto Salad Plate'));
+        $dao->insert(new Review('matt','Not Bad','Antipasto Salad Plate'));
+
+        $dao->insert(new Review('nick','Potato was too burnt','Baked Potato Bar'));
+        $dao->insert(new Review('abby','Awesome','Baked Potato Bar'));
+        $dao->insert(new Review('viola','Fantastic','Baked Potato Bar'));
+        $dao->insert(new Review('brock','Good','Baked Potato Bar'));
+
+        $dao->insert(new Review('brock','very beefy','BBQ Beef Briskett'));
+        $dao->insert(new Review('matt','decently beefy','BBQ Beef Briskett'));
+        $dao->insert(new Review('ryan','Terrible BBQ','BBQ Beef Briskett'));
+        $dao->insert(new Review('viola','Great','BBQ Beef Briskett'));
+
+        $dao->insert(new Review('sam','not good','BBQ Pulled Pork or Chicken Sandwhich'));
+        $dao->insert(new Review('vladimira','the chicken is great','BBQ Pulled Pork or Chicken Sandwhich'));
+        $dao->insert(new Review('nick','Good pork','BBQ Pulled Pork or Chicken Sandwhich'));
+        $dao->insert(new Review('stephen','Fantastic','BBQ Pulled Pork or Chicken Sandwhich'));
+
+
+        $dao->insert(new Review('michael','Too much cheese','Bean and Cheese Chimichanga'));
+        $dao->insert(new Review('matthew','too little cheese','Bean and Cheese Chimichanga'));
+        $dao->insert(new Review('viola','too much beans','Bean and Cheese Chimichanga'));
+        $dao->insert(new Review('abby','perfect','Bean and Cheese Chimichanga'));
+
+        $dao->insert(new Review('viola','not good','Bread Boules'));
+        $dao->insert(new Review('vladimira','great','Bread Boules'));
+        $dao->insert(new Review('brock','fantastic','Bread Boules'));
+        $dao->insert(new Review('matt','my favorite meal','Bread Boules'));
+
+
+        $dao->insert(new Review('sam','great for breakfast','Breakfast sandwich'));
+        $dao->insert(new Review('nick','really good eggs','Breakfast sandwich'));
+        $dao->insert(new Review('abby','great bacon egg and cheese','Breakfast sandwich'));
+        $dao->insert(new Review('stephen','the sausage is not so good','Breakfast sandwich'));
+
+        $dao->insert(new Review('stephen','aweful','Buffalo Chicken Penne'));
+        $dao->insert(new Review('viola','way too much buffalo sauce','Buffalo Chicken Penne'));
+        $dao->insert(new Review('abby','perfectly made','Buffalo Chicken Penne'));
+        $dao->insert(new Review('matthew','fantastic buffalo chicken','Buffalo Chicken Penne'));
+
     }
 }
 
 $dao = new ReviewDao();
 $dao->populate();
-$review = new Review('jorge','decent','Baked Potato Bar');
-$dao->insert($review);
+
 
